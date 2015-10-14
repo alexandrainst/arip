9f99e28ce1ecd701a258c907ccee4cd08bfc50df
lassevestergaard <lasse.vestergaard@alexandra.dk>
2015-10-14 16:40:33 +0200

    Added gitignore

---
+++ MobileFRONTEND/js/jquery.mobile.datebox.i18n.en_US.utf8.js
@@ -0,0 +1,118 @@
+/*
+ * jQuery-Mobile-DateBox 
+ * Date: Thu Aug 13 2015 17:31:26 UTC
+ * http://dev.jtsage.com/jQM-DateBox/
+ * https://github.com/jtsage/jquery-mobile-datebox
+ *
+ * Copyright 2010, 2015 JTSage. and other contributors
+ * Released under the MIT license.
+ * https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt
+ *
+ */
+jQuery.extend(jQuery.mobile.datebox.prototype.options.lang, { "en": {
+	"setDateButtonLabel": "Set Date",
+	"setTimeButtonLabel": "Set Time",
+	"setDurationButtonLabel": "Set Duration",
+	"calTodayButtonLabel": "Jump to Today",
+	"titleDateDialogLabel": "Choose Date",
+	"titleTimeDialogLabel": "Choose Time",
+	"daysOfWeek": [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+	],
+	"daysOfWeekShort": [
+		"Su",
+		"Mo",
+		"Tu",
+		"We",
+		"Th",
+		"Fr",
+		"Sa"
+	],
+	"monthsOfYear": [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	],
+	"monthsOfYearShort": [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	],
+	"durationLabel": [
+		"Days",
+		"Hours",
+		"Minutes",
+		"Seconds"
+	],
+	"durationDays": [
+		"Day",
+		"Days"
+	],
+	"tooltip": "Open Date Picker",
+	"nextMonth": "Next Month",
+	"prevMonth": "Previous Month",
+	"timeFormat": 12,
+	"headerFormat": "%A, %B %-d, %Y",
+	"dateFieldOrder": [
+		"m",
+		"d",
+		"y"
+	],
+	"timeFieldOrder": [
+		"h",
+		"i",
+		"a"
+	],
+	"slideFieldOrder": [
+		"y",
+		"m",
+		"d"
+	],
+	"dateFormat": "%-m/%-d/%Y",
+	"useArabicIndic": false,
+	"isRTL": false,
+	"calStartDay": 0,
+	"clearButton": "Clear",
+	"durationOrder": [
+		"d",
+		"h",
+		"i",
+		"s"
+	],
+	"meridiem": [
+		"AM",
+		"PM"
+	],
+	"timeOutput": "%l:%M %p",
+	"durationFormat": "%Dd %DA, %Dl:%DM:%DS",
+	"calDateListLabel": "Other Dates",
+	"calHeaderFormat": "%B %Y",
+	"calTomorrowButtonLabel": "Jump to Tomorrow"
+}});
+jQuery.extend(jQuery.mobile.datebox.prototype.options, {
+	useLang: "en"
+});
